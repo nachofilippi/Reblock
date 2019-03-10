5df32d43002dcb924a6d5fcf414db3b6b8144d76
nachofilippi <nachofilippi@live.com.ar>
2019-03-10 18:17:50 -0300

    componente para dirigir a asesoramiento finalizado

--- src/app/components/ref-asesoramiento/ref-asesoramiento.component.ts
+++ src/app/components/ref-asesoramiento/ref-asesoramiento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-ref-asesoramiento',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RefAsesoramientoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
-
+  btnClick = function () {
+    this.router.navigateByUrl('asesoramiento');
+  };
 }
