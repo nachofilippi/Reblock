8c4c3d66242d272cf29d68d12a66765ba4735945
nachofilippi <nachofilippi@live.com.ar>
2019-03-10 16:25:12 -0300

    mejoras generadas en el buscador y contacto. falta cte ppal asesoramient

--- src/app/components/buscador/buscador.component.ts
+++ src/app/components/buscador/buscador.component.ts
@@ -1,41 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {ProductosService} from '../../servicios/productos.service';
-import { Router }  from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-buscador',
   templateUrl: './buscador.component.html',
+  styleUrls: ['./buscador.component.css']
 })
 export class BuscadorComponent implements OnInit {
 
-    producto:any[] = []
-    productos:any[] = []
-    termino:string;
+    producto: any[] = [];
+    productos: any[] = [];
+    termino: string;
 
-  constructor( private activatedRoute:ActivatedRoute,
-                private _productosService:ProductosService,
+  constructor( private activatedRoute: ActivatedRoute,
+                private _productosService: ProductosService,
                 private router: Router) {
-                        this.activatedRoute.params.subscribe(params =>{
+                        this.activatedRoute.params.subscribe(params => {
                             // console.log(params['id']);
                             this.producto = this._productosService.getProducto ( params ['id']);
-                        })
+                        });
                }
 
 
   ngOnInit() {
 
-      this.activatedRoute.params.subscribe(params=>{
+      this.activatedRoute.params.subscribe(params => {
           // console.log(params['termino']);
-          this.termino= params['termino'];
-          this.productos =this._productosService.buscarProducto( params['termino']);
+          this.termino = params['termino'];
+          this.productos = this._productosService.buscarProducto( params['termino']);
           console.log(this.productos);
       });
   }
 
-  verProducto( idx: number ){
+  verProducto( idx: number ) {
     // console.log(idx);
-    this.router.navigate(['/producto',idx]);
+    this.router.navigate(['/producto', idx]);
   }
 
 }
